b0b087684d247186ab7a41b40856d3204aaf4a06
KimTran <tranhoang.thienkim.tk@gmail.com>
2025-01-25 23:49:57 +0800

    add mark and unmark features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,8 +4,8 @@ public class Duke {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Initialize the tasks array
-        String[] tasks = new String[100];
+        // Initialize the task list
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         // Display initial greeting
@@ -30,6 +30,7 @@ public class Duke {
             // If user types "list", display all tasks
             if (userInput.equalsIgnoreCase("list")) {
                 System.out.println("____________________________________________________________");
+                System.out.println("Here are the tasks in your list:");
                 if (taskCount == 0) {
                     System.out.println("No tasks added yet.");
                 } else {
@@ -41,8 +42,34 @@ public class Duke {
                 continue;
             }
 
-            // Add the input to the tasks array
-            tasks[taskCount] = userInput;
+            // If user types "mark", mark the task as done
+            if (userInput.startsWith("mark ")) {
+                int taskIndex = Integer.parseInt(userInput.substring(5)) - 1;
+                if (taskIndex >= 0 && taskIndex < taskCount) {
+                    tasks[taskIndex].markAsDone();
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("  " + tasks[taskIndex]);
+                    System.out.println("____________________________________________________________");
+                }
+                continue;
+            }
+
+            // If user types "unmark", mark the task as not done
+            if (userInput.startsWith("unmark ")) {
+                int taskIndex = Integer.parseInt(userInput.substring(7)) - 1;  // Fixed substring index
+                if (taskIndex >= 0 && taskIndex < taskCount) {
+                    tasks[taskIndex].markAsNotDone();
+                    System.out.println("____________________________________________________________");
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("  " + tasks[taskIndex]);
+                    System.out.println("____________________________________________________________");
+                }
+                continue;
+            }
+
+            // Add the task to the list
+            tasks[taskCount] = new Task(userInput);
             taskCount++;
 
             // Echo the input with confirmation

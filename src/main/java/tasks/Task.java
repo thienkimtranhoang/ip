ba50a77f7abe054b5b04d54bad105a1b49113f6e
KimTran <tranhoang.thienkim.tk@gmail.com>
2025-02-17 23:05:11 +0800

    implement save files to hard disk

--- src/main/java/tasks/Task.java
+++ src/main/java/tasks/Task.java
@@ -25,4 +25,33 @@ public class Task {
     public String toString() {
         return getStatusIcon() + " " + description;
     }
+
+    public String toFileFormat() {
+        return (this instanceof Todo ? "T" : this instanceof Deadline ? "D" : "E") +
+                " | " + (isDone ? "1" : "0") + " | " + description;
+    }
+
+    public static Task fromFileFormat(String line) {
+        String[] parts = line.split(" \\| ");
+        String type = parts[0];
+        boolean isDone = parts[1].equals("1");
+        String description = parts[2];
+
+        Task task = null;
+        switch (type) {
+        case "T":
+            task = new Todo(description);
+            break;
+        case "D":
+            task = new Deadline(description, parts[3]); // parts[3] is the deadline date
+            break;
+        case "E":
+            task = new Event(description, parts[3], parts[4]); // parts[3] is start, parts[4] is end
+            break;
+        }
+        if (task != null && isDone) {
+            task.markAsDone();
+        }
+        return task;
+    }
 }
